b4d75e03a38ca8b6a1b800fbea61bdf59cfe27b1
luisgustavo777 <hadockborracheiro2020@gmail.com>
2024-11-25 06:49:12 -0300

    adicionar feature de historia dos personagens

--- charactersCall/script.js
+++ charactersCall/script.js
@@ -1,3 +1,11 @@
+const characterHistories = {
+    "Ash Ketchum": "Ash Ketchum é o protagonista da série Pokémon, conhecido por sua busca para se tornar um mestre Pokémon.",
+    "James Rocket": "James é um membro da equipe Rocket, sempre tentando capturar Pikachu para o chefe Giovanni.",
+    "Misty": "Misty é a líder do ginásio de Cerulean e uma das primeiras amigas de Ash em sua jornada.",
+    "Brock": "Brock é o líder de ginásio de Pewter, especializado em Pokémon do tipo Pedra, e acompanha Ash em várias de suas aventuras.",
+    "Jessie Rocket": "Jessie é a outra integrante da equipe Rocket, sempre tentando atrapalhar as aventuras de Ash e seus amigos."
+};
+
 function hideAllTabs() {
     const pokemonContainer = document.getElementById("pokemon-container");
     const mapContainer = document.getElementById("map-container");
@@ -35,4 +43,18 @@ document.querySelectorAll(".character-card").forEach(function(card) {
 document.getElementById("character-modal-close").addEventListener("click", function() {
     document.getElementById("character-modal").style.display = "none";
     document.getElementById("sketchfab-embed-container").innerHTML = ""; 
+});
+
+document.querySelectorAll(".history-button").forEach(function(button) {
+    button.addEventListener("click", function() {
+        const characterName = button.dataset.character;
+        
+        document.getElementById("history-modal").style.display = "flex";
+        document.getElementById("history-modal-name").textContent = 'Sobre';
+        document.getElementById("history-modal-history").textContent = characterHistories[characterName] || "História não encontrada.";
+    });
+});
+
+document.getElementById("history-modal-close").addEventListener("click", function() {
+    document.getElementById("history-modal").style.display = "none";
 });
